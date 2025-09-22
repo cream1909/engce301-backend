3dfe4c7dd77ba0e4daa3f978e220f1bcefb97e79
cream1909 <wilasinee_wi661@live.rmutl.ac.th>
2025-09-22 14:13:26 +0700

    helmet

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // server.js - My First Express Server
 const express = require('express');
 const cors = require('cors'); // << Import cors
+const helmet = require('helmet'); // << Import helmet
 
 require('dotenv').config(); // << เพิ่มบรรทัดนี้ที่ด้านบน
 
 const app = express();
+app.use(helmet()); // << เพิ่มบรรทัดนี้: ใส่เกราะป้องกัน!
 app.use(cors()); // << เพิ่มบรรทัดนี้: ใช้ cors กับทุก request
 const PORT = process.env.PORT || 3000; // << อ่านค่า PORT จาก .env
 const APP_NAME = process.env.APP_NAME;
 
+
 // สร้าง Route (เส้นทาง) แรกสำหรับหน้าหลัก
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from ENGCE301 Express Server!</h1>');
+  res.send('<h1>Hello ${APP_NAME}!</h1>');
 });
 
+// เพิ่ม Route ใหม่สำหรับทดสอบ
 app.get('/api/data', (req, res) => {
     res.json({ message: 'This data is open for everyone!' });
 });
